fc9d6668f1da5244183188458be02504f5c97e6f
SieryVolk <lucadeias90@yahoo.it>
2023-11-24 20:56:28 +0100

    stampa a video della versione (aggiunta dependency package_info_plus

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_car/widgets/lista_sostituzioni.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  String version = packageInfo.version;
+  runApp(MyApp(version: version)); }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String version; // Definisci version come variabile di istanza
+  const MyApp({Key? key, required this.version}) : super(key: key);
 
 final String appTitle = 'My Car';
 
@@ -33,7 +41,26 @@ final String appTitle = 'My Car';
             )
           ),
         ),
-        body: const ListaSostituzioni(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Expanded(
+              child: ListaSostituzioni(), // Il corpo principale della pagina
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(0,0,24,48),
+              child: Text(
+                '$version \n Beta version',
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  color: Colors.grey.shade600,
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              )
+            )
+          ]
+        )
       )
     );
   }
